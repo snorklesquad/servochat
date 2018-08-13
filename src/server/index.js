4afc79708ee803c4faa97da458406bfb8d4269ab
athousandleaves <superamare@gmail.com>
2018-08-13 14:58:20 -0500

    Spin up server with sockets for realtime chat

---
+++ src/server/index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+const http = require('http').Server(app);
+const io = module.exports.io = require('socket.io')(http);
+
+const messages = [
+  {
+    username: 'welcome_bot',
+    text: 'Hello and welcome to the chat!'
+  }
+];
+var users = [];
+
+io.on('connection', socket => {
+  console.log('user connected: ', socket.id);
+  socket.emit('receive_id', socket.id);
+  io.emit('receive_message', messages); 
+  io.emit('receive_user', users); 
+
+  socket.on('send_message', data => {
+    messages.push(data);
+    io.emit('receive_message', messages); 
+  });
+
+  socket.on('send_user', data => {
+    users.push(data);
+    io.emit('receive_user', users); 
+  });
+
+  socket.on('disconnect', () => {
+    io.emit('disconnect_user', socket.id);
+    users.splice(users.indexOf(socket));
+    console.log('user disconnected: ', socket.id);
+  });
+});
+
+app.use(express.static(path.join(__dirname, '../client/')))
+
+app.get('*', (req, res) => {
+  res.redirect('/');
+});
+
+http.listen(process.env.PORT || 8080, () => console.log('on 8080'));
